d45b624b8681ab4461047670cc10b15b9409e30e
adamazmn <adamazman193@gmail.com>
2025-03-20 14:04:14 +0800

    Update signup3.component.ts

--- src/app/components/signup3/signup3.component.ts
+++ src/app/components/signup3/signup3.component.ts
@@ -8,3 +8,27 @@ import { RouterLink } from '@angular/router';
   styleUrl: './signup3.component.css',
 })
 export class Signup3Component {}
+personalDetailForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.personalDetailForm = this.fb.group({
+      fullName: [''],
+      dob: [''],
+      gender: ['Male'],
+      nationality: [''],
+      myKadNo: [''],
+      address: [''],
+      religion: [''],
+      frontPhoto: [null],
+      backPhoto: [null],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.personalDetailForm.value);
+  }
+
+  onRetake() {
+    this.personalDetailForm.reset();
+  }
+}
