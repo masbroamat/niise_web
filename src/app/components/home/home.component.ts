51bef09c699bca289a7a61246fc165589b8c25ee
KUROOP96 <haikalhakimi1214@gmail.com>
2025-03-20 12:37:43 +0800

    HomePage, Footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  quickAccessItems = [
+    { image: '/images/E-Pass.jpg', title: 'E-Pass' },
+    { image: '/images/DigitalLandingCard(DLC).png', title: 'Digital Landing Card (DLC)' },
+    { image: '/images/CustomDeclaration.png', title: 'Custom Declaration' },
+    { image: '/images/SSPI.jpg', title: 'SSPI' },
+    { image: '/images/MyBorderPass.jpg', title: 'MyBorder Pass' }
+  ];
+}
